d500b8c2b0d8b74599124088b68e86e81a37e67e
Scott A. Idler <scott.a.idler@gmail.com>
2023-03-09 14:27:52 -0800

    added initial space-age solution

---
+++ rust/space-age/src/lib.rs
@@ -0,0 +1,41 @@
+// The code below is a stub. Just enough to satisfy the compiler.
+// In order to pass the tests you can add-to or change any of this code.
+
+#[derive(Debug)]
+pub struct Duration(f64);
+
+impl From<u64> for Duration {
+    fn from(s: u64) -> Self {
+        Duration((s as f64) / (31557600.0 as f64))
+    }
+}
+
+pub trait Planet {
+    fn years_during(d: &Duration) -> f64 {
+        unimplemented!(
+            "convert a duration ({:?}) to the number of years on this planet for that duration",
+            d,
+        );
+    }
+}
+
+macro_rules! planet {
+    ($i:ident, $e:expr) => {
+        pub struct $i;
+        impl Planet for $i {
+            fn years_during(d: &Duration) -> f64 {
+                d.0 / $e
+            }
+        }
+        
+    };
+}
+
+planet!(Earth, 1.0);
+planet!(Mercury, 0.2408467);
+planet!(Venus, 0.61519726);
+planet!(Mars, 1.8808158);
+planet!(Jupiter, 11.862615);
+planet!(Saturn, 29.447498);
+planet!(Uranus, 84.016846);
+planet!(Neptune, 164.79132);
