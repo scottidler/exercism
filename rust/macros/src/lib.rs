7b0fb1e8bb8d3fde03dd59f65a7df1f6d54fb0f8
Scott A. Idler <scott.a.idler@gmail.com>
2023-03-09 18:25:21 -0800

    initial add of macros solution

---
+++ rust/macros/src/lib.rs
@@ -0,0 +1,15 @@
+#[macro_export]
+macro_rules! hashmap {
+    () => {
+        ::std::collections::HashMap::new()
+    };
+    ( $( $key:expr => $val:expr), + $(,)? ) => {
+        {
+            let mut map = ::std::collections::HashMap::new();
+            $(
+                map.insert($key, $val);
+            )*
+            map
+        }
+    };
+}
