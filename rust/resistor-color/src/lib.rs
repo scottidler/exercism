7b7178dfca09382ae75d99da0fc098e665407dba
Scott A. Idler <scott.a.idler@gmail.com>
2023-03-12 23:09:29 -0700

    cleaned up some code a bit

--- rust/resistor-color/src/lib.rs
+++ rust/resistor-color/src/lib.rs
@@ -1,5 +1,5 @@
-use int_enum::IntEnum;
-use enum_iterator::{all, cardinality, first, last, next, previous, reverse_all, Sequence};
+use enum_iterator::{all, Sequence};
+use int_enum::{IntEnum, IntEnumError};
 
 #[repr(u32)]
 #[derive(Clone, Copy, Debug, Eq, PartialEq, IntEnum, Sequence)]
@@ -16,35 +16,17 @@ pub enum ResistorColor {
     White = 9,
 }
 
-impl From<u32> for ResistorColor {
-    fn from(value: u32) -> Self {
-        match value {
-            0 => ResistorColor::Black,
-            1 => ResistorColor::Brown,
-            2 => ResistorColor::Red,
-            3 => ResistorColor::Orange,
-            4 => ResistorColor::Yellow,
-            5 => ResistorColor::Green,
-            6 => ResistorColor::Blue,
-            7 => ResistorColor::Violet,
-            8 => ResistorColor::Grey,
-            9 => ResistorColor::White,
-            _ => panic!("value out of range"),
-        }
-    }
-}
-
 pub fn color_to_value(color: ResistorColor) -> u32 {
-    color as u32
+    color.int_value()
 }
 
 pub fn value_to_color_string(value: u32) -> String {
-    match value {
-        0..=9 => format!("{:?}", ResistorColor::from(value)),
-        _ => String::from("value out of range"),
+    match ResistorColor::from_int(value) {
+        Ok(resistor) => format!("{:?}", resistor),
+        Err(_) => "value out of range".to_string()
     }
 }
 
 pub fn colors() -> Vec<ResistorColor> {
-    all::<ResistorColor>().collect::<Vec<_>>()
+    all::<ResistorColor>().collect()
 }
