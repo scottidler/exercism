96d9fc94aa67f3164cf0cd88a3b54c34f89471c4
Scott A. Idler <scott.a.idler@gmail.com>
2023-03-12 20:30:46 -0700

    initial add of rpg solution

---
+++ rust/role-playing-game/src/lib.rs
@@ -0,0 +1,33 @@
+pub struct Player {
+    pub health: u32,
+    pub mana: Option<u32>,
+    pub level: u32,
+}
+
+impl Player {
+
+    pub fn revive(&self) -> Option<Player> {
+        if self.health == 0 {
+            Some(Self {
+                health: 100,
+                mana: if self.level >= 10 { Some(100) } else { None },
+                level: self.level,
+            })
+        } else {
+            None
+        }
+    }
+
+    pub fn cast_spell(&mut self, mana_cost: u32) -> u32 {
+        let mut damage = 0;
+        if let Some(mana) = self.mana {
+            if mana >= mana_cost {
+                self.mana = Some(mana - mana_cost);
+                damage = mana_cost * 2
+            }
+        } else {
+            self.health = self.health.saturating_sub(mana_cost);
+        } 
+        damage
+    }
+}
